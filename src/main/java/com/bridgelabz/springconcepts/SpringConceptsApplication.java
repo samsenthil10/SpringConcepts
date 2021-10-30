e5a699a4a0dabea9645be88a48ca960ecb9736f7
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-30 06:13:27 +0530

    Added: Logger to log messages

--- src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.springconcepts;
 
 import com.bridgelabz.springconcepts.component.DemoBean;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,12 +10,14 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringConceptsApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to spring concept demo");
+
+		logger.debug("Welcome to spring concept demo");
 		System.out.println();
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		DemoBean demoBean = context.getBean(DemoBean.class);
 		System.out.println();
-		System.out.println("Demo Bean ="+demoBean.toString());
+		logger.debug("Checking context: ={}",context.getBean(DemoBean.class));
 	}
 }
